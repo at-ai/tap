4712d700f9d23dc2c0cba8e30d0e3dbde67de55a
Alex Vizcaino <avizcaino@uxland.es>
2020-12-11 15:35:14 +0100

    feat: load excel

--- packages/calculator/src/calculator.ts
+++ packages/calculator/src/calculator.ts
@@ -3,12 +3,14 @@ import { Student } from "./domain";
 import { students } from "./students";
 import { getCriteriaDimensions } from "./utilities/get-criteria-dimensions";
 import { getMarksMean } from "./utilities/get-marks-mean";
+const excelToJson = require("convert-excel-to-json");
+const fs = require("fs");
 
 export const calculate = (student: Student) => {
   const toCriteriaDimensions = Object.keys(student.criteria).reduce(
     (cd, c) =>
       cd.concat(
-        getCriteriaDimensions(c).map((d) => ({
+        getCriteriaDimensions(c)?.map((d) => ({
           dimension: d,
           mark: student.criteria[c],
         }))
@@ -16,15 +18,40 @@ export const calculate = (student: Student) => {
     []
   );
   const groupDimensions = _.groupBy(toCriteriaDimensions, "dimension");
-  const alpha = Object.keys(groupDimensions).reduce(
+  const marks = Object.keys(groupDimensions).reduce(
     (dimensions, d) => ({
       ...dimensions,
-      [d]: getMarksMean(groupDimensions[d].map((value) => value.mark)),
+      [d]: getMarksMean(groupDimensions[d].map((value) => value?.mark)),
     }),
     {}
   );
-  console.log(alpha);
-  return alpha;
+  return marks;
 };
 
-calculate(students[0]);
+const result = excelToJson({
+  sourceFile: __dirname + "/marks.xlsx",
+  sheets: ["sheet1"],
+})?.sheet1;
+
+const headers = result[0]; // _.invert(result[0]);
+result.shift();
+const mappedStudents = result.map((r) =>
+  Object.keys(r).reduce((mapped: any, k) => {
+    if (k == "A" || k == "B" || k == "C")
+      return { ...mapped, [headers[k]]: r[k] };
+    else
+      return {
+        ...mapped,
+        criteria: { ...(mapped.criteria || {}), [headers[k]]: r[k] },
+      };
+  }, {})
+);
+
+const studentMarks = mappedStudents.map((s) => ({
+  name: s.Nom,
+  firstSurname: s["Primer Cognom"],
+  secondSurname: s["Segon Cognom"],
+  // marks: calculate(s),
+  ...calculate(s),
+}));
+console.table(studentMarks);
